defb1f5df391de8d3eb6d81ec07b29d4577f2dd1
SArcD <141703135+SArcD@users.noreply.github.com>
2025-03-18 01:31:40 -0600

    Update mai.py

--- mai.py
+++ mai.py
@@ -1043,6 +1043,15 @@ elif pagina == "Análisis por base":
         with st.expander("**Validación del modelo**"):
         # Matriz de confusión
             st.subheader("📌 Matriz de Confusión")
+            ## 📌 Matriz de Confusión
+            st.markdown(""" Muestra los aciertos y errores del modelo de clasificación comparando predicciones con valores reales.
+
+            - **TP (True Positive)**: Predicciones correctas de la clase positiva.
+            - **FP (False Positive)**: Casos incorrectamente clasificados como positivos.
+            - **FN (False Negative)**: Casos incorrectamente clasificados como negativos.
+            - **TN (True Negative)**: Predicciones correctas de la clase negativa.
+            """)
+
             st.write(pd.DataFrame(confusion_matrix(y_test_original, y_pred_original),
                       index=[f"Actual {reverse_mapping[c]}" for c in sorted(y.unique())],
                       columns=[f"Predicho {reverse_mapping[c]}" for c in sorted(y.unique())]))
