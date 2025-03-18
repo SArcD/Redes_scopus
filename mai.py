043e82cdc91054502da86004ab1ba71f20f4f1c2
SArcD <141703135+SArcD@users.noreply.github.com>
2025-03-17 19:13:53 -0600

    Update mai.py

--- mai.py
+++ mai.py
@@ -218,8 +218,8 @@ elif pagina == "Análisis por base":
             st.write("📋 **Resumen de autores agrupados**")
             st.dataframe(df_grouped)
             with st.expander("Datos del archivo"):
-                st.write(f"**Número de filas:** {df_grouped}.shape[0]}")
-                st.write(f"**Número de columnas:** {df_grouped}.shape[1]}")
+                st.write(f"**Número de filas:** {df_grouped.shape[0]}")
+                st.write(f"**Número de columnas:** {df_grouped.shape[1]}")
                 st.write("**Lista de columnas:**")
                 st.write(df_grouped.columns.tolist())  # Mostrar los nombres de las columnas como una lista
 
