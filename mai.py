6e6e3d23e96f5ec3691ab103cb4ef26612ca0f00
SArcD <141703135+SArcD@users.noreply.github.com>
2025-03-21 20:14:54 -0600

    Update mai.py

--- mai.py
+++ mai.py
@@ -1237,7 +1237,7 @@ elif pagina == "Análisis por base":
             st.markdown("### Comparativa con tu Cluster")
 
             # Ejemplo: mostrar estadísticas comparativas
-            st.dataframe(df_cluster.describe())  # Solo como ejemplo
+            #st.dataframe(df_cluster.describe())  # Solo como ejemplo
         
             st.markdown("""
             En la siguiente tabla puede verse la comparación entre los datos especificados por el usuario, los integrantes del Cluster que guarda la mayor similitud con estos datos y con los autores de la base de datos completa. Las columnas de interés son: el parámetro a comparar (ya sea el número de publicaciones, citas, la antigüedad o la porporción de publicaciones financiadas), los datos introducidos por el usuario, la media, el cuartil 1 y el cuartil 3 del cluster que corresponda, las medias, cuartil 1 y cuartil 3 de la base completa.
