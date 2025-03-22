464ecb774fc7f3460b03583921c6207a6a6c16c3
SArcD <141703135+SArcD@users.noreply.github.com>
2025-03-22 12:35:19 -0600

    Update mai.py

--- mai.py
+++ mai.py
@@ -32,7 +32,7 @@ if pagina == "Inicio":
 Esta aplicación está ideada para la visualización y análisis de la producción científica de los investigadores de la Universidad de Colima. En su versión mas reciente, se utilizan los datos disponibles en la base de [Scopus](https://www.scopus.com), la cual indexa artículos, libros y patentes derivados del trabajo científico en todo el mundo. Se examinan tendencias de publicación, áreas de conocimiento predominantes, colaboración nacional e internacional, autores más productivos, revistas preferidas, y el impacto de las publicaciones.
 """)
 
-st.markdown("""
+    st.markdown("""
 El objetivo es proporcionar una herramienta interactiva que permita a investigadores, tomadores de decisiones y público interesado explorar visualmente la evolución y características de la actividad científica institucional.
 
 Este análisis puede contribuir a:
