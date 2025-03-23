e8b805e801ee806e1ee5d3361defe4925e6c9296
SArcD <141703135+SArcD@users.noreply.github.com>
2025-03-22 23:37:03 -0600

    Update mai.py

--- mai.py
+++ mai.py
@@ -359,7 +359,7 @@ elif pagina == "Análisis por base":
             # Reordenar el DataFrame con las columnas prioritarias primero
             df_grouped = df_grouped[priority_columns + remaining_columns]
             # Crear columna Folio tipo UCOL-0001, UCOL-0002, ...
-            df_grouped['Folio'] = ['UCOL-' + str(i).zfill(4) for i in range(1, len(df) + 1)]
+            df_grouped['Folio'] = ['UCOL-' + str(i).zfill(4) for i in range(1, len(df_grouped) + 1)]
 
             # Crear diccionario de correspondencia entre Folio y Author(s) ID
             diccionario_folios = dict(zip(df_grouped['Folio'], df_grouped['Author(s)_ID']))
